157a0124dbde4575bf924fad514ba7152aa8ac69
Sergey Rastrigin <srastrigin@gmail.com>
2017-02-07 12:38:56 +0000

    reset project

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-    let router = require('express').Router();
-    let home = require('./home')(router,app);
-    let login = require('./login')(router,app);
-    let register = require('./register')(router,app);
-    let logout = require('./logout')(router,app);
-};
--- routes/logout.js
+++
@@ -1,9 +0,0 @@
-module.exports = (router, app) => {
-
-    router.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-
-    app.use('/logout', router);
-}
